9bdb8dbc708f35d5575115112f8e076f07747e0d
William McCotter <bjmccotter7192@gmail.com>
2018-05-04 16:44:30 -0500

    IT WORKS

---
+++ cashman-flask-project/cashman/model/transaction.py
@@ -0,0 +1,18 @@
+import datetime
+from marshmallow import Schema, fields
+
+class Transaction():
+    def __init__(self, description, amount, type):
+        self.description = description
+        self.amount = amount
+        self.type = type
+    
+    def __repr__(self):
+        return '<Transaction(name={self.description!r})>'.format(self=self)
+
+
+class TransactionSchema(Schema):
+    description = fields.Str()
+    amount = fields.Str()
+    created_at = fields.Date()
+    type = fields.Str()
